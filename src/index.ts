24a1c626f8b9c5aa31b91bb5a301754fd2c5a40a
Oladayo Ahmod <57647734+Oladayo-Ahmod@users.noreply.github.com>
2023-12-11 17:34:45 +0000

    create user functionality

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
-import { $query,int, $update,text,Principal,float64, Record, 
-    StableBTreeMap, Vec, match, Result, nat64, ic, Opt }
+import { query,int, update,text,Principal,float64, Record, 
+    StableBTreeMap, Vec, Result, nat64, ic, Opt, Err, Ok, Canister }
  from 'azle';
 import { v4 as uuidv4 } from 'uuid';
 
@@ -50,7 +50,44 @@ const trackerStorage : typeof Tracker = {
     users : []
 }
 
-const incomeStorage = StableBTreeMap(Principal,Income,1) //income storage
-const expenseStorage = StableBTreeMap(Principal,Expenses,21) // expense storage
+let currentUser : typeof User | undefined
 
+const userStorage = StableBTreeMap(Principal,User,1);
+const incomeStorage = StableBTreeMap(Principal,Income,2) //income storage
+const expenseStorage = StableBTreeMap(Principal,Expenses,3) // expense storage
+
+export default Canister({
+// create new user
+createUser: update(
+  [text, text, float64],
+  Result(text, text),
+  (username :any, password : string, amount :number) => {
+
+    const user = userStorage
+      .values()
+      .filter((c: typeof User) => c.username === username)[0];
+    if (user) {
+      return Err('user already exists.');
+    }
+    const newUser: typeof User = {
+      id: idGenerator(),
+      username,
+      password,
+      amount,
+    };
+    userStorage.insert(newUser.id, newUser);
+    return Ok(`${newUser.username} added successfully.`);
+  }
+),
+
+})
+
+// ID generator
+function idGenerator(): Principal {
+  const randomBytes = new Array(29)
+    .fill(0)
+    .map((_) => Math.floor(Math.random() * 256));
+
+  return Principal.fromUint8Array(Uint8Array.from(randomBytes));
+}
 
